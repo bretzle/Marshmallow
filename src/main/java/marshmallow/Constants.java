39ed80b921d3908ae8bc62f91d2cdcec5c3b11da
John <johnfish218@gmail.com>
2019-07-03 17:21:45 -0500

    Added link to github in Constants

--- src/main/java/marshmallow/Constants.java
+++ src/main/java/marshmallow/Constants.java
@@ -6,4 +6,6 @@ public class Constants {
     public static final long ACTIVITY_LOG_CHANNEL_ID = 548589935159017473L;
     public static final long CHANGELOG_CHANNEL_ID = 548589935159017473L;
     public static final long FEEDBACK_CHANNEL_ID = 548589935159017473L;
+
+    public static final String SOURCE_URI = "https://github.com/bretzle/Marshmallow/tree/master/src/main/java/marshmallow/commands/%s/%s.java";
 }
