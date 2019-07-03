eb7686a481bac137e5eefdd09d3566c911ec63ef
John <johnfish218@gmail.com>
2019-07-03 17:24:27 -0500

    Added base command and ping command

--- src/main/java/marshmallow/commands/Command.java
+++ src/main/java/marshmallow/commands/Command.java
@@ -1,4 +1,62 @@
 package marshmallow.commands;
 
-public class Command {
+import marshmallow.Marshmallow;
+
+import java.util.List;
+import java.util.Objects;
+
+public abstract class Command {
+
+    protected final Marshmallow marshmallow;
+    protected final boolean allowDM;
+
+    public Command(Marshmallow marshmallow) {
+        this(marshmallow, true);
+    }
+
+    public Command(Marshmallow marshmallow, boolean allowDM) {
+        this.marshmallow = marshmallow;
+        this.allowDM = allowDM;
+    }
+
+    public abstract String getName();
+
+    public abstract String getDescription();
+
+    public abstract List<String> getUsageInstructions();
+
+    public abstract List<String> getExampleUsage();
+
+    public abstract List<Class<? extends Command>> getRelations();
+
+    public abstract List<String> getTriggers();
+
+    public Category getCategory() {
+        return null;
+    }
+
+    public abstract List<CommandGroup> getGroups();
+
+    public final boolean isAllowDM() {
+        return allowDM;
+    }
+
+    public abstract boolean onCommand(Context context, String[] args);
+
+    private boolean isSame(Command command) {
+        return Objects.equals(command.getName(), getName())
+                && Objects.equals(command.getUsageInstructions(), getUsageInstructions())
+                && Objects.equals(command.getExampleUsage(), getExampleUsage())
+                && Objects.equals(command.getTriggers(), getTriggers());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Command && isSame((Command) obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this);
+    }
 }
--- src/main/java/marshmallow/commands/utility/PingCommand.java
+++ src/main/java/marshmallow/commands/utility/PingCommand.java
@@ -1,13 +1,82 @@
 package marshmallow.commands.utility;
 
+import marshmallow.Marshmallow;
 import marshmallow.commands.Command;
+import marshmallow.commands.CommandGroup;
+import marshmallow.commands.Context;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PingCommand extends Command {
 
-    public PingCommand() {
-        List<String> list = new ArrayList<>();
+    public PingCommand(Marshmallow marshmallow) {
+        super(marshmallow);
+    }
+
+    @Override
+    public String getName() {
+        return "Ping Command";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Can be used to check if the bot is still alive";
+    }
+
+    @Override
+    public List<String> getUsageInstructions() {
+        return Collections.singletonList("`:command` - Returns the latency of the bot.");
+    }
+
+    @Override
+    public List<String> getExampleUsage() {
+        return null;
+    }
+
+    @Override
+    public List<Class<? extends Command>> getRelations() {
+        return null;
+    }
+
+    @Override
+    public List<String> getTriggers() {
+        return Collections.singletonList("Ping");
+    }
+
+    @Override
+    public List<CommandGroup> getGroups() {
+        return null;
+    }
+
+    @Override
+    public boolean onCommand(Context context, String[] args) {
+        long start = System.currentTimeMillis();
+        context.message.getChannel().sendTyping().queue(v -> {
+            long ping = System.currentTimeMillis() - start;
+
+            context.makeInfo(context.i18n("message"))
+                    .set("heartbeat", context.getJDA().getPing())
+                    .set("rating", ratePing(context, ping))
+                    .set("ping", ping)
+                    .queue();
+        });
+        return true;
+    }
+
+    private String ratePing(Context context, long ping) {
+        if (ping <= 10) return context.i18n("rating.10");
+        if (ping <= 100) return context.i18n("rating.100");
+        if (ping <= 200) return context.i18n("rating.200");
+        if (ping <= 300) return context.i18n("rating.300");
+        if (ping <= 400) return context.i18n("rating.400");
+        if (ping <= 500) return context.i18n("rating.500");
+        if (ping <= 600) return context.i18n("rating.600");
+        if (ping <= 700) return context.i18n("rating.700");
+        if (ping <= 800) return context.i18n("rating.800");
+        if (ping <= 900) return context.i18n("rating.900");
+        if (ping <= 1600) return context.i18n("rating.1600");
+        if (ping <= 10000) return context.i18n("rating.10000");
+        return context.i18n("rating.other");
     }
 }
